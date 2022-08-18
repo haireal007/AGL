59a50a5c1399d3ebd24e4e252445ffda0c97cdfb
haireal007 <you@example.com>
2022-08-18 13:51:50 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product} from "../models/Product";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AGULAR';
+  title = 'crudAngular';
+  products: Product[] = [];
+  product: Product = new Product(0, "", "", 0);
+
+
+  constructor() {
+  }
+
+  create(): void {
+    this.products.push(this.product);
+    localStorage.setItem("index2", String(this.product.id));
+  }
+
+  delete(index: number): void {
+    this.products.splice(index, 1);
+  }
+
+  save(i: number): void {
+    localStorage.setItem("index", String(i));
+    this.product = this.products[i];
+  }
+
+  edit(): void {
+    let index = localStorage.getItem("index");
+    this.product = this.products[Number(index)];
+  }
+
+  reset(): void {
+    if (this.products.length == 0) {
+      this.product = new Product(1, "", "", 0);
+    } else {
+      let id = localStorage.getItem("index2");
+      this.product = new Product(Number(id) + 1, "", "", 0);
+    }
+  }
+
 }
